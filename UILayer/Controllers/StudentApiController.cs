4a7e48ec0b74b9eb72e8e5139509edb007c5b7af
remo <remo@gmail.com>
2024-01-09 17:08:53 +0530

    'iniitialdeploy'

---
+++ UILayer/Controllers/StudentApiController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace UILayer.Controllers
+{
+    public class StudentApiController : ApiController
+    {
+        [HttpGet]
+        [Route("GetStudent")]
+        public IHttpActionResult Getstudents()
+        {
+            List<string> rec = new List<string>() { "manoj", "suresh" };
+
+            return Ok(rec);
+        }
+    }
+}
---
+++ UILayer/Controllers/StudentUIController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UILayer.Controllers
+{
+    public class StudentUIController : Controller
+    {
+        // GET: StudentUI
+        public ActionResult AddStudent()
+        {
+            return View();
+        }
+    }
+}
